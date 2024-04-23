64c17cfe21fb27d1d336ee9e079f3cc49611bb84
Christopher <92882782+Krostofer@users.noreply.github.com>
2024-04-23 10:49:22 -0400

    Initial commit

---
+++ com/example/Fibonacci.java.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Christopher Di Bert
+ * @version 1.0
+ * @since 2024-04-23
+ */
+
+// Fibonacci.java class
+public final class Fibonacci.java {
+
+    /** Private constructor to prevent instantiation. */
+    private Fibonacci.java() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
